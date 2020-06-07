42dc93b115869dbd0ded718002591e77c69459e0
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2020-06-07 18:06:56 +0300

    Issue #39: use qrandomgenerator instead of qrand with Qt >= 5.10

--- QEverCloud/src/OAuth.cpp
+++ QEverCloud/src/OAuth.cpp
@@ -31,6 +31,10 @@
 #include <QWebHistory>
 #endif
 
+#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
+#include <QRandomGenerator>
+#endif
+
 #include <cstring>
 
 /** @cond HIDDEN_SYMBOLS  */
@@ -43,17 +47,25 @@ quint64 random64()
 {
     quint64 res = 0;
     for(int i = 0; i < 8; i++) {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
+        res += (QRandomGenerator::global()->generate64() % 256) << i*8;
+#else
         res += static_cast<quint64>(qrand() % 256) << i*8;
+#endif
     }
 
     QByteArray randomData = QUuid::createUuid().toRfc4122();
     quint64 random;
     std::memcpy(&random, &randomData.constData()[0], sizeof(random));
-    res ^= random;
-    std::memcpy(&random, &randomData.constData()[sizeof(random)],
-                sizeof(random));
+
     res ^= random;
 
+    std::memcpy(
+        &random,
+        &randomData.constData()[sizeof(random)],
+        sizeof(random));
+
+    res ^= random;
     return res;
 }
 
