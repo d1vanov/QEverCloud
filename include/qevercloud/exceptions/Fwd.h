e8d34e2ca74d9f1db31f152494f45cb5bdf7f4ee
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2021-03-29 19:13:17 +0300

    Update generated code: add Fwd headers

---
+++ include/qevercloud/exceptions/Fwd.h
@@ -0,0 +1,26 @@
+/**
+ * Original work: Copyright (c) 2014 Sergey Skoblikov
+ * Modified work: Copyright (c) 2015-2021 Dmitry Ivanov
+ *
+ * This file is a part of QEverCloud project and is distributed under the terms
+ * of MIT license:
+ * https://opensource.org/licenses/MIT
+ *
+ * This file was generated from Evernote Thrift API
+ */
+
+#ifndef QEVERCLOUD_GENERATED_EXCEPTIONS_FWD_H
+#define QEVERCLOUD_GENERATED_EXCEPTIONS_FWD_H
+
+class EDAMInvalidContactsException;
+class EDAMNotFoundException;
+class EDAMSystemException;
+class EDAMSystemExceptionAuthExpired;
+class EDAMSystemExceptionRateLimitReached;
+class EDAMUserException;
+class EverCloudException;
+class EvernoteException;
+class NetworkException;
+class ThriftException;
+
+#endif // QEVERCLOUD_GENERATED_EXCEPTIONS_FWD_H
---
+++ include/qevercloud/services/Fwd.h
@@ -0,0 +1,20 @@
+/**
+ * Original work: Copyright (c) 2014 Sergey Skoblikov
+ * Modified work: Copyright (c) 2015-2021 Dmitry Ivanov
+ *
+ * This file is a part of QEverCloud project and is distributed under the terms
+ * of MIT license:
+ * https://opensource.org/licenses/MIT
+ *
+ * This file was generated from Evernote Thrift API
+ */
+
+#ifndef QEVERCLOUD_GENERATED_SERVICES_FWD
+#define QEVERCLOUD_GENERATED_SERVICES_FWD
+
+class INoteStore;
+class IUserStore;
+class NoteStoreServer;
+class UserStoreServer;
+
+#endif // QEVERCLOUD_GENERATED_SERVICES_FWD
