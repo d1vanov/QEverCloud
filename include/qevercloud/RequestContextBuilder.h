79825ebc1d0f9338f0a05fe368d43a78c6d1ab31
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2022-08-24 18:04:27 +0300

    Add RequestContextBuilder class for convenience of IRequestContextPtr creation

---
+++ include/qevercloud/RequestContextBuilder.h
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2022 Dmitry Ivanov
+ *
+ * This file is a part of QEverCloud project and is distributed under the terms
+ * of MIT license: https://opensource.org/licenses/MIT
+ */
+
+#ifndef QEVERCLOUD_REQUEST_CONTEXT_BUILDER_H
+#define QEVERCLOUD_REQUEST_CONTEXT_BUILDER_H
+
+#include <qevercloud/Export.h>
+#include <qevercloud/Fwd.h>
+
+#include <QNetworkCookie>
+#include <QString>
+
+#include <memory>
+
+namespace qevercloud {
+
+class QEVERCLOUD_EXPORT RequestContextBuilder
+{
+public:
+    RequestContextBuilder();
+
+    RequestContextBuilder & setAuthenticationToken(QString authenticationToken);
+    RequestContextBuilder & setRequestTimeout(qint64 timeoutMsec);
+
+    RequestContextBuilder & setIncreaseRequestTimeoutExponentially(
+        bool increaseRequestTimeoutExponentially);
+
+    RequestContextBuilder & setMaxRequestTimeout(qint64 timeoutMsec);
+    RequestContextBuilder & setMaxRetryCount(quint32 maxRetryCount);
+    RequestContextBuilder & setCookies(QList<QNetworkCookie> cookies);
+
+    [[nodiscard]] IRequestContextPtr build();
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> m_impl;
+};
+
+} // namespace qevercloud
+
+#endif // QEVERCLOUD_REQUEST_CONTEXT_BUILDER_H
---
+++ src/RequestContextBuilder.cpp
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2022 Dmitry Ivanov
+ *
+ * This file is a part of QEverCloud project and is distributed under the terms
+ * of MIT license: https://opensource.org/licenses/MIT
+ */
+
+#include <qevercloud/RequestContextBuilder.h>
+#include <qevercloud/RequestContext.h>
+
+namespace qevercloud {
+
+class RequestContextBuilder::Impl
+{
+public:
+    void reset()
+    {
+        m_authenticationToken.clear();
+        m_requestTimeout = DEFAULT_REQUEST_TIMEOUT_MSEC;
+        m_increaseRequestTimeoutExponentially =
+            DEFAULT_REQUEST_TIMEOUT_EXPONENTIAL_INCREASE;
+
+        m_maxRequestTimeout = DEFAULT_MAX_REQUEST_TIMEOUT_MSEC;
+        m_maxRetryCount = DEFAULT_MAX_REQUEST_RETRY_COUNT;
+        m_cookies.clear();
+    }
+
+    QString m_authenticationToken;
+    qint64 m_requestTimeout = DEFAULT_REQUEST_TIMEOUT_MSEC;
+
+    bool m_increaseRequestTimeoutExponentially =
+        DEFAULT_REQUEST_TIMEOUT_EXPONENTIAL_INCREASE;
+
+    qint64 m_maxRequestTimeout = DEFAULT_MAX_REQUEST_TIMEOUT_MSEC;
+    quint32 m_maxRetryCount = DEFAULT_MAX_REQUEST_RETRY_COUNT;
+    QList<QNetworkCookie> m_cookies = {};
+};
+
+RequestContextBuilder::RequestContextBuilder() :
+    m_impl{std::make_unique<Impl>()}
+{}
+
+RequestContextBuilder & RequestContextBuilder::setAuthenticationToken(
+    QString authenticationToken)
+{
+    m_impl->m_authenticationToken = std::move(authenticationToken);
+    return *this;
+}
+
+RequestContextBuilder & RequestContextBuilder::setRequestTimeout(
+    const qint64 timeoutMsec)
+{
+    m_impl->m_requestTimeout = timeoutMsec;
+    return *this;
+}
+
+RequestContextBuilder & RequestContextBuilder::setIncreaseRequestTimeoutExponentially(
+    const bool increaseRequestTimeoutExponentially)
+{
+    m_impl->m_increaseRequestTimeoutExponentially =
+        increaseRequestTimeoutExponentially;
+
+    return *this;
+}
+
+RequestContextBuilder & RequestContextBuilder::setMaxRequestTimeout(
+    const qint64 timeoutMsec)
+{
+    m_impl->m_maxRequestTimeout = timeoutMsec;
+    return *this;
+}
+
+RequestContextBuilder & RequestContextBuilder::setMaxRetryCount(
+    const quint32 maxRetryCount)
+{
+    m_impl->m_maxRetryCount = maxRetryCount;
+    return *this;
+}
+
+RequestContextBuilder & RequestContextBuilder::setCookies(
+    QList<QNetworkCookie> cookies)
+{
+    m_impl->m_cookies = std::move(cookies);
+    return *this;
+}
+
+IRequestContextPtr RequestContextBuilder::build()
+{
+    auto ctx = newRequestContext(
+        std::move(m_impl->m_authenticationToken),
+        m_impl->m_requestTimeout,
+        m_impl->m_increaseRequestTimeoutExponentially,
+        m_impl->m_maxRequestTimeout,
+        m_impl->m_maxRetryCount,
+        std::move(m_impl->m_cookies));
+
+    m_impl->reset();
+    return ctx;
+}
+
+} // namespace qevercloud
