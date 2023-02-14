3256fbf8fb596b5d79dafd2e060b960455c4d461
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2023-02-14 22:39:37 +0300

    Remove old thumbnail downloader implementation

--- include/qevercloud/QEverCloud.h
+++ include/qevercloud/QEverCloud.h
@@ -1,6 +1,6 @@
 /**
  * Original work: Copyright (c) 2014 Sergey Skoblikov
- * Modified work: Copyright (c) 2015-2020 Dmitry Ivanov
+ * Modified work: Copyright (c) 2015-2023 Dmitry Ivanov
  *
  * This file is a part of QEverCloud project and is distributed under the terms
  * of MIT license: https://opensource.org/licenses/MIT
@@ -12,9 +12,9 @@
 #include "DurableService.h"
 #include "EventLoopFinisher.h"
 #include "IInkNoteImageDownloader.h"
+#include "INoteThumbnailDownloader.h"
 #include "NetworkProxy.h"
 #include "RequestContext.h"
-#include "Thumbnail.h"
 #include "EDAMErrorCode.h"
 #include "Constants.h"
 #include "exceptions/All.h"
